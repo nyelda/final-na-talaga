a4f15e819bab2d6732b98280163ee5856d5aab0e
qkdtjayme <qkdtjayme@tip.edu.ph>
2024-02-22 23:58:50 +0800

    modified

--- frontend/src/components/homepage/Healthy.js
+++ frontend/src/components/homepage/Healthy.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../Navbar.css';
+import '../../Navbar.css';
 
 const Healthy = () => {
     const [activeTab, setActiveTab] = useState('exercise');
--- frontend/src/components/homepage/Obese.js
+++ frontend/src/components/homepage/Obese.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../Navbar.css';
+import '../../Navbar.css';
 
 const Obese = () => {
     const [activeTab, setActiveTab] = useState('exercise');
